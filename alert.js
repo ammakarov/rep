d36cf954dc8f1dd2a1dc563de535b9f93e8343b1
ammakarov <am_makarov@mail.ru>
2017-02-02 20:52:52 +0300

    1 попытка

---
+++ alert.js
@@ -0,0 +1,13 @@
+/**
+ * Created by user on 01.02.2017.
+ */
+var admin, name
+name = 'Василий';
+admin = name;
+// alert('Я - JavaScript');
+// alert('Привет');
+alert(admin);
+var myPlanet = 'Земля';
+var vizitorName = 'Петя';
+alert(1/0);
+
