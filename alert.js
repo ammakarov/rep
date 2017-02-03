6f6dbfd9c735bff69ee2b9e39a54341c146e4799
ammakarov <am_makarov@mail.ru>
2017-02-03 14:07:45 +0300

    add

--- alert.js
+++ alert.js
@@ -1,18 +1,37 @@
 /**
  * Created by user on 01.02.2017.
  */
-var admin, name
+/*var admin, name
 name = 'Василий';
-admin = name;
+admin = name;*/
 // alert('Я - JavaScript');
 // alert('Привет');
 alert(admin);
-var myPlanet = 'Земля';
+/*var myPlanet = 'Земля';
 var vizitorName = 'Петя';
 alert(1/0);
 var i, a, b, c;
 a=1;
+b=3;*/
 b=3;
+
+var objects = [
+    {key: 3,
+    y: 0.5,
+    title: "456"},
+    {key: 5,
+    f: 8,
+    title: "rte"},
+    {key: 8,
+    x: 3,
+    title:"ups"}
+];
+
+// вывести title, у которого key == 8
+
+
+// посчитать сумму значений key
+
 
 
 alert("проверка дальнего срабатывания");
