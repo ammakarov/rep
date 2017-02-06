ce1e902b736b812fe112bb8304d42d1a3a2a83cc
ammakarov <am_makarov@mail.ru>
2017-02-06 21:52:37 +0300

    task if

---
+++ task.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Aleksej on 06/02/17.
+ */
+//var name = '';
+//name = prompt('Как тебя зовут?', '')
+//alert(prompt('Как тебя зовут?', ''));
+/*
+var otvet = '';
+otvet = prompt("Каково \"официальное\" название JavaScript?", '');
+if(otvet == 'EcmaScript')
+{
+    alert('Верно');
+}
+else
+{
+    alert('Не знаете? "EcmaScript"!')
+}*/
+var namber = prompt('Введите число', '');
+if (namber > 0)
+    alert('1');
+else if (namber < 0)
+    alert('-1');
+else
+    alert('0');
