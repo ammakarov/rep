69b999c35672302dbc7af5f4ab042537ed6a848b
ammakarov <am_makarov@mail.ru>
2017-02-07 16:57:59 +0300

    task if

--- task.js
+++ task.js
@@ -16,10 +16,30 @@ else
     alert('Не знаете? "EcmaScript"!')
 }*/
 
-var namber = prompt('Введите число', '');
+/*var namber = prompt('Введите число', '');
 if (namber > 0)
     alert('1');
 else if (namber < 0)
     alert('-1');
 else
-    alert('0');
+    alert('0');*/
+
+/*var login = '', pass = '';
+login = prompt('Кто пришел ?', '');
+if(login == null)
+    alert('Вход отменен');
+else if(login != 'Админ')
+          alert('Я вас не знаю');
+    else {
+        pass = prompt('Пароль ?', '');
+        if (pass == null)
+            alert('Ввод отменен');
+        else if (pass != 'Черный Властелин')
+                alert('Пароль не верен');
+            else
+                alert('Добро пожаловать!');
+    }*/
+
+
+
+
