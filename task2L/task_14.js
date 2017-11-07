1fd9921fe0baca0257fe55e60266e8f6f7ad1353
ammakarov <am_makarov@mail.ru>
2017-11-07 14:55:50 +0300

    task 14

---
+++ task2L/task_14.js
@@ -0,0 +1,12 @@
+function EvenOrOdd(number) {
+    if(number%2) {
+        return "Odd";
+    } else {
+        return "Even";
+    }
+};
+alert( EvenOrOdd(0) );
+alert( EvenOrOdd(1) );
+alert( EvenOrOdd(101) );
+alert( EvenOrOdd(20) );
+alert( EvenOrOdd(-111) );
