4666ce8cd7fff883d12f8bea200a4fcfe983615c
ammakarov <am_makarov@mail.ru>
2017-11-13 15:34:37 +0300

    task 3

--- task2L/task_3.js
+++ task2L/task_3.js
@@ -1,1 +1,4 @@
-var jsonTxt = "{\"key1\":\"value1\", \"key2\" : [\"value2\", \"value3\"]}}"
+var jsonTxt = "{\"key1\":\"value1\",\"key2\":[\"value2\",\"value3\"]}";
+var obj = JSON.parse(jsonTxt);
+var mass =  obj.key2;
+alert(mass);
